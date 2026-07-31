3990dff7eea7abdfb19be554dd5f784f1347aff9
therajesh1 <therajeshchheda@gmail.com>
2026-07-31 11:02:28 +0530

    Add build.sh for Render build process

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
+
+# Seed database
+python manage.py seed_brands
+python manage.py seed_products
